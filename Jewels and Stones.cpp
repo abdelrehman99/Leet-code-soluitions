08f90c715e963f8a8b1e2ef65389cd8d928cc877
abdelrehman99 <56692870+abdelrehman99@users.noreply.github.com>
2021-08-03 18:14:49 +0200

    Add files via upload

---
+++ Jewels and Stones.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define F first
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define WL(t) while(t --)
+#define S second
+#define PB push_back
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define runtime() cerr << ((double)clock() / CLOCKS_PER_SEC)
+#define MP make_pair
+#define llu unsigned long long
+typedef long long ll;typedef long double ld;
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+
+
+
+int numJewelsInStones(string jewels, string stones) 
+    {
+        long c = 0;
+        for (int i = 0; i < jewels.size(); i++)
+        {
+            for (int j = 0; j < stones.size(); j++)
+            {
+                if (jewels[i] == stones[j])
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+
+
+
+
+signed main()
+{
+    fast;
+    int t = 1;
+    //cin >> t;
+    string j, s;
+    WL(t)
+    {
+        cin >> j >> s;
+    	cout << numJewelsInStones(j, s);
+        runtime();
+    }
+    return 0;
+}
